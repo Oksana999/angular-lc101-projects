fcc7801144644b30bd3d5f69e7a115af979178f6
Oksana999 <oksanam999@gmail.com>
2020-07-29 00:56:58 -0500

    Display the text "Work harder!" Display trophyImage under the Done Chores list. Otherwise, display the text, "No allowance yet." Display an underneath the lists with the text "Ice cream treat!" Otherwise, display how to earn ice cream

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -1,15 +1,18 @@
+// @ts-ignore
 import { Component, OnInit } from '@angular/core';
 
+// @ts-ignore
 @Component({
    selector: 'chores',
    templateUrl: './chores.component.html',
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = [ 'Clean the house'];
+   finishedChores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries', 'Clean the house'];
 
-   trophyImage = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
+   /* trophyImage = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';*/
+     trophyImage = 'https://st2.depositphotos.com/1032712/5873/i/450/depositphotos_58737691-stock-photo-gold-cup-winner-trophy-3d.jpg';
 
    constructor() { }
 
